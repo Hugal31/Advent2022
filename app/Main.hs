0e7c7f012ac5a73a357f37d58e805dcd540c3010
Hugo Laloge <hla@lescompanions.com>
2022-12-03 11:10:39 +0100

    Improve main

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,6 @@ module Main where
 import Advent2022.Days
 import Text.Printf
 import System.Environment (getArgs)
-import System.IO
 
 main :: IO ()
 main = do
@@ -17,12 +16,8 @@ parseDayAndPart _ = error "Invalid args"
 
 runDayAndPart :: Int -> Int -> IO ()
 runDayAndPart day part = do
-    handle <- openFile (dayInputFile day) ReadMode
-    content <- hGetContents handle
+    content <- readFile (dayInputFile day)
     printf "Day %d part %d:\n%d" day part $ Advent2022.Days.getDayPart day part content
-    -- Note: I do not close the handle, because otherwise I get issues because I use it lazily.
-    -- TODO Look at https://stackoverflow.com/questions/7867723/haskell-file-reading
-
 
 dayInputFile :: Int -> String
 dayInputFile = printf "inputs/day%02d.txt"
